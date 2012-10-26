eba9882376a0d6f78e07d8037bde4e13a6973e4e
Armando Couto <armando-couto@hotmail.com>
2012-10-26 17:11:22 -0300

    Finalizado.
    Signed-off-by: Armando Couto <armando-couto@hotmail.com>

--- Projetos Calculo Numerico/src/NP_2/InterpolacaoPolinomial.java
+++ Projetos Calculo Numerico/src/NP_2/InterpolacaoPolinomial.java
@@ -6,55 +6,42 @@ import javax.swing.JOptionPane;
 
 /**
  * Método de Interpolação Polinomial.
- * 
- * @author Armando Couto Bibliografia:
- *         http://wwwp.fc.unesp.br/~arbalbo/Iniciacao_Cientifica
- *         /interpolacao/teoria/1_Interpol_polinomial_Met_Lagrange_Newton.pdf
- *         
- *         http://dexter-one.net/programacion/codigo-fuente-en-java-para-la-resolucion-de-metodos-numericos/
+ * @author Armando Couto 
+ * Bibliografia:
+ * http://dexter-one.net/programacion/codigo-fuente-en-java-para-la-resolucion-de-metodos-numericos/
  */
 public class InterpolacaoPolinomial implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	Integer tamanhoVetor;
-	double xx[][] = new double[3][3];
-	// double x[];
-	// double y[];
-	// double a[];
-	double x2[];
-	double somatorio = 1;
-
-	// Só para testes.
-	double x[] = { -1, 0, 2 };
-	double y[] = { 4, 1, -1 };
-	double a[] = new double[3];
-
-	private void solInterpolacaoPolinomial() {
-		tamanhoVetor = Integer.parseInt(JOptionPane.showInputDialog(null,
-				"Tamanho dos Vetores:"));
-		// x[] = new double[tamanhoVetor];
-		// y[] = new double[tamanhoVetor];
-		// a[] = new double[tamanhoVetor];
-		// for (int i = 0; i < 3; i++) {
-		// x[i] = Double.parseDouble(JOptionPane.showInputDialog(null,
-		// "Digite o valor X"+i+":"));
-		// y[i] = Double.parseDouble(JOptionPane.showInputDialog(null,
-		// "Digite o valor Y"+i+":"));
-		// }
-
-		for (int i = 0; i < x.length; i++) {
-			xx[i][0] = 1;
-			for (int j = 1; j < y.length; j++)
-				xx[i][j] = Math.pow(x[i], j);
+	int n;
+	double x[];
+	double y[];
+	double xx[];
+	double valorX;
+	
+	private void metodoPolinomial() {
+		
+		n = Integer.parseInt(JOptionPane.showInputDialog(null, "Tamanho dos Vetores:"));
+		x = new double[n];
+		y = new double[n];
+		for (int i = 0; i < n; i++) {
+			x[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor X" + i + ":"));
+			y[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor Y" + i + ":"));
 		}
-
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-				System.out.println("X" + i + "Y" + (j) + " = " + xx[i][j]);
+		valorX = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor de X:"));
+		n = x.length - 1;
+		xx = (double[]) y.clone();
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n - i; ++j) {
+				xx[j] = (valorX - x[j]) / (x[i + j + 1] - x[j]) * xx[j + 1]
+						+ (valorX - x[i + j + 1]) / (x[j] - x[i + j + 1]) * xx[j];
+			}
+		}
+		System.out.println("Valor da Interpolação: " + xx[0]);
 	}
 
 	public static void main(String[] args) {
-		new InterpolacaoPolinomial().solInterpolacaoPolinomial();
+		new InterpolacaoPolinomial().metodoPolinomial();
 	}
 }
